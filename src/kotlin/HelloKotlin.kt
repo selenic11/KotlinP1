5d89e8a78a5a1c968507ccfcba773ddafeb70eee
Elena Mir <selenic11@mail.ru>
2021-10-31 18:26:15 +0300

    add IJ project

---
+++ src/kotlin/HelloKotlin.kt
@@ -0,0 +1,8 @@
+fun main() {
+    //one comment
+    println("Hello! My name's Lena. I'm a teacher.")
+    /*second
+    comment*/
+    print("I want ")
+    print("to become a cool mobile app developer.")
+}
